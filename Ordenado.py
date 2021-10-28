a22d02edbe0abc339ec25700286090176d945773
nuno muletas <79775803+MuletasNuno@users.noreply.github.com>
2021-10-28 18:40:26 +0100

    Create Ordenado.py

---
+++ Ordenado.py
@@ -0,0 +1,16 @@
+nomeAluno = input("Nome: ")
+nota = int(input("Qual a nota que o aluno teve 0-200: "))
+
+print("Nome: ", nomeAluno)
+print("Nota: ", nota)
+
+
+if (nota > 200):
+    print("Nota invalida")
+elif (nota >= 95):
+    print("O aluno passou")    
+elif (nota < 95):
+        print("O aluno reprovou")
+else:
+    print("Nota invalida")
+
