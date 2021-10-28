548ad1545680bb038a5007889cd6c6c52d55022c
João Esteves <joaoesteves10A4@sapo.pt>
2021-10-28 18:30:23 +0100

    Update and rename calculadora to calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+print("Insira o primeiro número:")
+numA = input()
+
+print("Insira o segundo numero:")
+numB= input()
+
+operador = "Escolha a operação: {}, {}, {}, {}, {}")
+print(operador("+","-","x","/", "%"))
+operador= input()
+
+try:
+    if(variavel_operador == "+"):
+        print(numA + numB)
+    else if(variavel_operador == "*"):
+        print(numA * numB)
+    else if(variavel_operador == "-"):
+        print(numA - numB)
+    else if(variavel_operador == "/"):
+        print(numA / numB)
+    else if(variavel_operador == "%"):
+        print(numA % numB)
+    else:
+        print("operação inválida")
+expect Exception as err:
+        print("ERRO: " + err.error())
